b741e0c39c334e17c840cc1978c8bde7e2716c30
Pavan Gaikwad <gaikwadpavan57@gmail.com>
2026-01-15 20:21:19 +0530

    change

--- Backend/src/main/java/com/example/quizzapp/controller/UserController.java
+++ Backend/src/main/java/com/example/quizzapp/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.quizzapp.model.Questions;
@@ -19,13 +20,14 @@ public class UserController {
 	@Autowired
 	private QuestionsService questionsService;
 	
-	@GetMapping("category/{category}")
-	public ResponseEntity<List<Questions>> playQuiz(
-            @PathVariable String category) {
+	 @GetMapping("/category/{category}")
+    public ResponseEntity<List<Questions>> playQuiz(
+            @PathVariable String category,
+            @RequestParam(defaultValue = "10") int limit
+    ) {
+        System.out.println("🔥 PLAY CONTROLLER HIT: " + category + " limit=" + limit);
 
-		System.out.println("🔥 PLAY CONTROLLER HIT with category = " + category);
-		
-        return questionsService.getQuestionsByCategory(category);
+        return questionsService.getQuestionsByCategory(category, limit);
     }
 	
 }
