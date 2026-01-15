e6c9952071e5c770ac4d92d5d1faa3701783569f
Pavan Gaikwad <gaikwadpavan57@gmail.com>
2026-01-15 20:21:45 +0530

    achange

--- Backend/src/main/java/com/example/quizzapp/dao/QuestionsDao.java
+++ Backend/src/main/java/com/example/quizzapp/dao/QuestionsDao.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.quizzapp.model.Questions;
@@ -16,6 +17,19 @@ public interface QuestionsDao extends JpaRepository<Questions, Integer> {
 	
 	List<Questions> findByCategory(String category);
 	
-    @Query(value = "SELECT * FROM questions q Where q.category=:category ORDER BY RAND() LIMIT numQ", nativeQuery = true)
-	List<Questions> findRandomQuestionsByCategory(String category, int numQ);
+	
+	 @Query(
+      value = """
+        SELECT *
+        FROM questions
+        WHERE category = :category
+        ORDER BY RAND()
+        LIMIT :numQ
+      """,
+      nativeQuery = true
+    )
+    List<Questions> findRandomQuestionsByCategory(
+        @Param("category") String category,
+        @Param("numQ") int numQ
+    );
 }
